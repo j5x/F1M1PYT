6d6e3a828b643e0fa9e764bda8bc62cb5b174752
j5x <32881@ma-web.nl>
2021-09-27 12:48:27 +0200

    SchoolTool

---
+++ SchoolTool.py
@@ -0,0 +1,11 @@
+
+
+print("Welke dag is het vandaag")
+HuidigeDag = input()
+
+if HuidigeDag == "Maandag" or HuidigeDag == "Dinsdag" or HuidigeDag == "Woensdag" or HuidigeDag == "Donderdag" or HuidigeDag == "Vrijdag":
+    print("Pak je spullen en ga naar school")
+elif HuidigeDag == "maandag" or HuidigeDag == "dinsdag" or HuidigeDag == "woensdag" or HuidigeDag == "donderdag" or HuidigeDag == "vrijdag":
+    print("Pak je spullen en ga naar school")
+else: 
+    print("Lekker uit slapen")
