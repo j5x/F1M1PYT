aeb31d233464566af4f5c4bfe37194516d9dfbf7
j5x <32881@ma-web.nl>
2021-09-30 12:53:23 +0200

    F1M1PYT

---
+++ PYTB1L2MyFirstOperations.py
@@ -0,0 +1,30 @@
+a = 1+1
+print(a)
+a = a+3
+print(a)
+a = a+a*a
+print(a)
+a = a-10
+print(a)
+a-=10
+print(a)
+
+c = 10/2
+print(c)
+
+b = 10%4
+print(b)
+
+count = 0
+count += 1
+print(count)
+
+ccount = 1
+ccount /= 100
+print(ccount)
+
+getal = 4
+
+getal /= 2
+
+print(getal)
---
+++ PYTB1L2PlayerStatistics.py
@@ -0,0 +1,18 @@
+name = "Jason Siegersma"
+jumpforce = 60
+speed = 310
+Height = 1.92
+Lives = "Immortal"
+Level = 'unkown'
+Strength = "Inhuman"
+Specialability = 'Time/Space'
+playername = "Tempest"
+
+print("naam: "+name)
+print("jumpforce: " + str(jumpforce))
+print("speed: "+ str(speed))
+print("Height(meter): "+ str (Height))
+print("Lives: "+ (Lives))
+print("Strength: "+ (Strength))
+print("Specialability: "+ (Specialability))
+print("Player Name: "+ (playername)) 
