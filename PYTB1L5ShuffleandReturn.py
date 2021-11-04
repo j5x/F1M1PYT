1f52febe01ce7de8210f172d5190a1afedf81a99
j5x <32881@ma-web.nl>
2021-11-04 12:09:09 +0100

    fc

---
+++ PYTB1L5ShuffleandReturn.py
@@ -0,0 +1,10 @@
+import random
+original = "hello you"
+rdm = ''.join(random.sample(original,len(original)))
+print(rdm)
+original = "hello us"
+rdm = ''.join(random.sample(original,len(original)))
+print(rdm)
+original = "hello me"
+rdm = ''.join(random.sample(original,len(original)))
+print(rdm)
